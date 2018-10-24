81ead1058bb7303a523054f20bc2b6c05a5b17ff
Lukas Strobl <267028@via.dk>
2018-10-24 14:08:24 +0200

    aa
    qq

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("andrius ir lukas dx");
             System.out.println("andrius ir lukas dx");
             System.out.println("andrius ir lukas dx");System.out.println("andrius ir lukas dx");System.out.println("andrius ir lukas dx");
-
+            System.out.println("andrius ir lukas dxs");
 
         }
 
