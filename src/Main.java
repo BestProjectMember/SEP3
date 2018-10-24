e66ce7e25307e607af3a83f5d7cff87aa512f8c6
2 <266899@via.dk>
2018-10-24 13:40:08 +0200

    more hello asdsadsdfgdfdsfdsvfdvdfvfdvd

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,27 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World Friends :)");
 
-        System.out.println("Hello World Friends :)");System.out.println("Hello World Friends :)");System.out.println("Hello World Friends :)");
         System.out.println("Hello World Friends :)");
-        
+        System.out.println("Hello World Friends :)");System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+        System.out.println("Hello World Friends :)");
+
+
+
+
+
+
+
+
+
+
 
 
 
