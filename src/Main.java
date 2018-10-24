16ca898d00010ba2cd61b3e8a46f275f5f645159
Zujevas <266916@via.dk>
2018-10-24 14:40:05 +0300

    test inteliJ

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Hello World Friends :)");System.out.println("Hello World Friends :)");System.out.println("Hello World Friends :)");
         System.out.println("Hello World Friends :)");
+        System.out.println("Eikit nx dx kurva autistai:)");
         
 
 
