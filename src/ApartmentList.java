00139984ab23feab67e76a1ae78ce0bdd120e799
Lukas Jurgilas <jurgilasslukas@gmail.com>
2018-10-25 14:34:18 +0200

    Exceptions are back

--- src/ApartmentList.java
+++ src/ApartmentList.java
@@ -7,10 +7,21 @@ public class ApartmentList {
     public ApartmentList() {
         list = new ArrayList<Apartment>();
     }
+    
+        public void addApartment(Apartment apartment) throws Exception {
+            // checking if there is no apartment with same number
+            for(int i = 0; i < list.size(); i++)
+            {
+                if(list.get(i).getNumber() == apartment.getNumber())
+                    throw new Exception("Apartment with this number is already in the system");
+            }
 
-    public void addApartment(Apartment apartment) {
-        list.add(apartment);
-    }
+            // checking if all fields are filled
+            if(apartment.getLocation().equals(""))
+                throw new Exception("Make sure to enter the location");
+
+            list.add(apartment);
+        }
 
     public void removeApartment(int number) {
         for (int i = 0; i<list.size(); i++) {
