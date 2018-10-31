49a3b34920876cd46e86d356b13c529a09e4eeaa
luakso <38975851+luakso@users.noreply.github.com>
2018-10-31 12:30:54 +0100

    addint to main

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,28 @@
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        ApartmentList list = new ApartmentList();
+        ApartmentList adminList = new ApartmentList();
         Apartment apartment = new Apartment(11, "1233", 1, true, 10.2, 1 );
         Apartment apartment1 = new Apartment(13, "1233", 2, true, 10.2, 1 );
         Apartment apartment2 = new Apartment(14, "1233", 1, true, 10.2, 1 );
+        TenantList tenantList = new TenantList();
+        Tenant tenant1 = new Tenant("lukas", "asdasd", "EC1234412",  new Date(11/1/1998), "adsasd@via.dk", "+4550505050", "dog");
+
 
         try {
-            list.addApartment(apartment);
-            list.addApartment(apartment1);
-            list.addApartment(apartment2);
+            adminList.addApartment(apartment);
+            adminList.addApartment(apartment1);
+            adminList.addApartment(apartment2);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        list.removeApartment(14);
 
-        System.out.println(list.getAllApartments() + "\n");
+
+        System.out.println(adminList.getAllApartments() + "\n");
 
     }
 }
